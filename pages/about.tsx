f2e495fbd3f213bcc7ad16e1b89cb3991253b84e
Back <back4936nantawat@gmail.com>
2022-07-27 18:45:29 +0700

    first commit

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+
+}
+
+function about({ }: Props): ReactElement {
+    return (
+        <div>
+            <h1>test</h1>
+        </div>
+    )
+}
+
+export default about
