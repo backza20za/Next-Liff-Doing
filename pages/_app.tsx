5897777b67f94647075dde5154e1afe4594abffe
Back <back4936nantawat@gmail.com>
2022-07-28 16:23:05 +0700

    test login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   pageProps.liffError = liffError;
   return (
     <>
-      {pageProps.liff && pageProps.liff.isLoggedIn() ? <Component {...pageProps} /> : pageProps.liff.login(pageProps.liff.login({ redirectUri: "https://next-liff-app.herokuapp.com/" }))}
+      {pageProps.liff && pageProps.liff.isLoggedIn() ? <Component {...pageProps} /> : pageProps.liff.login({ redirectUri: "https://next-liff-app.herokuapp.com/" })}
       {pageProps.liffError && <Component {...pageProps} />}
       {/* <Component {...pageProps} /> */}
     </>
