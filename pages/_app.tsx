6904c4c7506a822a90e929161ee8382067b39bdd
Back <back4936nantawat@gmail.com>
2022-07-28 16:28:47 +0700

    test login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   pageProps.liffError = liffError;
   return (
     <>
-      <Component {...pageProps} />
+      {liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID }, <Component {...pageProps} />, <Component {...pageProps} />)}
+      {/* <Component {...pageProps} /> */}
     </>
   );
 }
