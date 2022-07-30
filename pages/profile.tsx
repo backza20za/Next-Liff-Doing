706f42aacc0d187b5b2e2d173939ab8090a66a57
Back <back4936nantawat@gmail.com>
2022-07-31 00:05:05 +0700

    edit profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,5 +1,6 @@
 import type { Liff } from "@line/liff";
 import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 import * as React from 'react';
 import Table from '@mui/material/Table';
@@ -37,15 +38,38 @@ const Profile: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
 }) => {
     return (
         <Layout>
-            <main>
+            <main className={styles.main}>
                 <h1>Profile</h1>
                 {liff && (
-                    <>
-                        {/* <p>{liff.isLoggedIn()}</p> */}
-                        {liff.isLoggedIn() === true ? <p>true</p> : <p>false</p>}
-                        <p>{liff.getOS()}</p>
-
-                    </>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Dessert (100g serving)</TableCell>
+                                    <TableCell align="right">Calories</TableCell>
+                                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow
+                                        key={row.name}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="right">{row.calories}</TableCell>
+                                        <TableCell align="right">{row.fat}</TableCell>
+                                        <TableCell align="right">{row.carbs}</TableCell>
+                                        <TableCell align="right">{row.protein}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 )
                 }
                 {liffError && (
@@ -64,35 +88,7 @@ const Profile: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
                     LIFF Documentation
                 </a>
             </main>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                                <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+
         </Layout>
     );
 };
