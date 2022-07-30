435da1e2c4ddf684ac4307bf0d2b754a30a367cc
Back <back4936nantawat@gmail.com>
2022-07-30 14:38:56 +0700

    add store

---
+++ store/Slices/bookSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../app/store'
+
+interface bookState {
+    bookName: string,
+    bookPrice: number,
+    bookImage: string
+}
+
+const initialState: bookState = {
+    bookName: "",
+    bookPrice: 0,
+    bookImage: ""
+}
+
+const fetchUserById = createAsyncThunk(
+    'users/fetchById',
+    // if you type your function argument here
+    async (userId: number) => {
+        const response = await fetch(`https://reqres.in/api/users/${userId}`)
+        return (await response.json()) as Returned
+    }
+)
+
+const bookSlice = createSlice({
+    name: 'book',
+    initialState,
+    reducers: {
+        // fill in primary logic here
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchUserById.pending, (state, action) => {
+            // both `state` and `action` are now correctly typed
+            // based on the slice state and the `pending` action creator
+        })
+    },
+})
+
+export default usersSlice.reducer
+
