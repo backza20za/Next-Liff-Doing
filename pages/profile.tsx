1111e85c95f903f68e916b860cc1f0c627c7ac42
Back <back4936nantawat@gmail.com>
2022-07-30 23:56:37 +0700

    profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import type { Liff } from "@line/liff";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import React, { useEffect } from 'react'
+import Layout from "../components/Layout";
 
-type Props = {}
-
-const profile = (props: Props) => {
+const Profile: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
+    liff,
+    liffError
+}) => {
     return (
-        <div>profile</div>
-    )
-}
+        <Layout>
+            <main className={styles.main}>
+                <h1>Profile</h1>
+                {liff && (
+                    <>
+                        {/* <p>{liff.isLoggedIn()}</p> */}
+                        {liff.isLoggedIn() === true ? <p>true</p> : <p>false</p>}
+                        <p>{liff.getOS()}</p>
 
-export default profile
+                    </>
+                )
+                }
+                {liffError && (
+                    <>
+                        <p>LIFF init failed.</p>
+                        <p>
+                            <code>{liffError}</code>
+                        </p>
+                    </>
+                )}
+                <a
+                    href="https://developers.line.biz/ja/docs/liff/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    LIFF Documentation
+                </a>
+            </main>
+        </Layout>
+    );
+};
+
+export default Profile;
