cd526b6ecfc8deb7323270f0ba4936e1953d59ec
Back <back4936nantawat@gmail.com>
2022-08-01 16:12:41 +0700

    dummy data

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -23,6 +23,16 @@ export default function handler(
           name: "React",
           bookImg: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7200/9781720043997.jpg",
           bookPrice: 200
+        },
+        {
+          name: "Vue",
+          bookImg: "https://www.b2s.co.th/media/catalog/product/cache/31c1e2dc995f93131e503292bcd47775/5/3/5303110.jpg",
+          bookPrice: 150
+        },
+        {
+          name: "Angular",
+          bookImg: "https://www.b2s.co.th/media/catalog/product/cache/31c1e2dc995f93131e503292bcd47775/5/2/5290075_1.jpg",
+          bookPrice: 300
         }
       ]
     }
