1ed206b57f8b8a03c7c018f6245ea338f7c9d26e
Back <back4936nantawat@gmail.com>
2022-08-01 17:32:19 +0700

    call api get book

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -20,17 +20,17 @@ export default function handler(
     {
       books: [
         {
-          name: "React",
+          bookName: "React",
           bookImg: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7200/9781720043997.jpg",
           bookPrice: 200
         },
         {
-          name: "Vue",
+          bookName: "Vue",
           bookImg: "https://www.b2s.co.th/media/catalog/product/cache/31c1e2dc995f93131e503292bcd47775/5/3/5303110.jpg",
           bookPrice: 150
         },
         {
-          name: "Angular",
+          bookName: "Angular",
           bookImg: "https://www.b2s.co.th/media/catalog/product/cache/31c1e2dc995f93131e503292bcd47775/5/2/5290075_1.jpg",
           bookPrice: 300
         }
