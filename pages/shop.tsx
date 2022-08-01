33eb3b0f6be96749770954e087f46c9f7ad1e673
Back <back4936nantawat@gmail.com>
2022-08-02 01:12:42 +0700

    rollback

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -6,13 +6,7 @@ import Layout from "../components/Layout";
 import { useSelector } from 'react-redux'
 import { bookSelector } from '../store/Slices/bookSlice'
 import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+
 
 
 
@@ -28,35 +22,6 @@ const Shop: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
                 {liff && (
                     <>
                         <h1>Shop</h1>
-                        <TableContainer component={Paper}>
-                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Dessert (100g serving)</TableCell>
-                                        <TableCell align="center">Calories</TableCell>
-                                        <TableCell align="center">Fat&nbsp;(g)</TableCell>
-
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {getBooks.addToCard.map((data, index) => (
-                                        <TableRow
-                                            key={index}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <TableCell component="th" scope="row">
-                                                <Image src={data.image} width={50} height={50} />
-                                            </TableCell>
-                                            <TableCell align="right">{data.name}</TableCell>
-                                            <TableCell align="right">{data.price}</TableCell>
-
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-
-
                     </>
                 )
                 }
