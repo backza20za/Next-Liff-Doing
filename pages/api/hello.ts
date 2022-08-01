2d9e7056efa576bcef3a4cec5aebbaed99d19336
Back <back4936nantawat@gmail.com>
2022-08-01 15:59:35 +0700

    dummy api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type Books = {
+  name: string,
+  bookImg: string,
+  bookPrice: number
+}
+
 type Data = {
-  name: string
+  books: Books[]
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    {
+      books: [
+        {
+          name: "React",
+          bookImg: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7200/9781720043997.jpg",
+          bookPrice: 200
+        }
+      ]
+    }
+  )
 }
